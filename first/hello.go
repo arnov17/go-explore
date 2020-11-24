75841dd5d49c4d877bde9ce26a4a946ee299133b
arnov17 <arnov.julian17@gmail.com>
2020-11-24 21:55:30 +0700

    first go

---
+++ first/hello.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello World")
+	fmt.Println("Satu =", 1)
+	fmt.Println("Dua =", 2)
+	fmt.Println("Tiga Koma Lima =", 3.5)
+	fmt.Println("benar =", true)
+	fmt.Println("benar =", false)
+	fmt.Println(len("Arnov"))
+	fmt.Println("Arnov"[0]) // byte
+}
+
+//compile kdengan 'go build <filename>.go
+// lihat hasil compile ls -l, then ./<filename>
+// ketika app sudah jadi perlu di compile
+
+// go run <filename>.go
---
+++ first/varb.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Arnov"
+	fmt.Println(name)
+
+	name = "Julian"
+	fmt.Println(name)
+
+	var friendName = "Ani"
+	fmt.Println(friendName)
+
+	var age = 26
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Eko"
+		lastName  = "Khannedy"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
